cc1656892486fd51bb927dd69e2fcae29cc6efa4
Luiz Felipe <raziel_libertino@hotmail.com>
2021-01-27 15:37:30 -0400

    logo e config fonts

--- snkquiz/pages/index.js
+++ snkquiz/pages/index.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components'
 import db from '../db.json'
+import Head from 'next/head'
 import Widget from '../src/components/Widget'
 import QuizBackground from '../src/components/QuizBackground'
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
+import QuizLogo from '../src/components/QuizLogo'
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -25,7 +27,11 @@ export const QuizContainer = styled.div`
 export default function Home () {
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Shingeki on Kyojin - QUIZ</title>
+      </Head>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1>Shingeki on Kyojin</h1>
